977d216d054b89a54db8e8b73aa78f9257fb047c
almeidabbm <almeidabatistam@gmail.com>
2022-08-06 16:57:54 +0100

    Add powerlevel10k configuration

--- link.sh
+++ link.sh
@@ -1,3 +1,4 @@
 ln -nfs $HOME/Develop/dot-files/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -nfs $HOME/Develop/dot-files/.config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
 ln -nfs $HOME/Develop/dot-files/.zshrc $HOME/.zshrc
+ln -nfs $HOME/Develop/dot-files/.p10k.zsh $HOME/.p10k.zsh
