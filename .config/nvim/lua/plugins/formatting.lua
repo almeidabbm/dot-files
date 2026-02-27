fcec60c1269275b7a1d9d4340535489338ff9509
almeidabbm <almeidabatistam@gmail.com>
2026-02-27 18:32:06 +0000

    chore: use native oxfmt

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -30,15 +30,6 @@ return {
         graphql = { "prettier" },
         lua = { "stylua" },
       },
-      formatters = {
-        oxfmt = {
-          command = "oxfmt",
-          args = { "--stdin-filepath", "$FILENAME" },
-          stdin = true,
-          -- Resolve from project node_modules first
-          cwd = require("conform.util").root_file({ "package.json" }),
-        },
-      },
       format_on_save = {
         timeout_ms = 3000,
         lsp_format = "fallback",
