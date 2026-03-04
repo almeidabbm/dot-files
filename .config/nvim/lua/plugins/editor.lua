76240eb451b50da717d9a631642f74abff4035d0
almeidabbm <almeidabatistam@gmail.com>
2026-03-04 15:33:28 +0000

    chore: add find in file

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -101,6 +101,7 @@ return {
       { "<leader>fb", "<cmd>Telescope buffers<cr>",     desc = "Buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>",   desc = "Help" },
       { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+      { "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search in buffer" },
     },
     config = function()
       local telescope = require("telescope")
